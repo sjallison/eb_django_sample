e75d27863c2f793f36ab893e53d96d37eeb6c75c
vbourachot <vbourachot@gmail.com>
2015-08-23 21:03:45 -0400

    Restore default urls config to display debug banner at /

--- django_eb/django_eb/urls.py
+++ django_eb/django_eb/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = patterns('', 
+# urlpatterns = patterns('',
+#     url(r'^admin/', include(admin.site.urls)),
+# ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
